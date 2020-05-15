a718bcf3b81d38e74a6038eb7cd240e43a7c5e23
Mike Walters <mike@flomp.net>
2020-05-15 22:37:10 +0100

    receiver: add vendor request to write radio SPI registers

--- gateware/receiver.py
+++ gateware/receiver.py
@@ -11,12 +11,16 @@ import time
 
 import usb1
 
-from nmigen                          import Elaboratable, Module
+from nmigen                          import *
+from usb_protocol.types              import USBRequestType
 from usb_protocol.emitters           import DeviceDescriptorCollection
 
 from luna                            import top_level_cli
 from luna.gateware.usb.usb2.device   import USBDevice
 from luna.gateware.usb.usb2.endpoint import USBStreamInEndpoint
+from luna.gateware.usb.usb2.request  import USBRequestHandler
+
+from radio                           import RadioSPI
 
 
 VENDOR_ID  = 0x16d0
@@ -33,6 +37,63 @@ TEST_TRANSFER_SIZE = 16 * 1024
 # have scheduled at a given time.
 TRANSFER_QUEUE_DEPTH = 16
 
+class RadioSPIRequestHandler(USBRequestHandler):
+    """ Read/write radio SPI registers. """
+
+    REQUEST_WRITE_REG = 0
+
+    def __init__(self):
+        super(RadioSPIRequestHandler, self).__init__()
+        self.spi_start       = Signal()
+        self.spi_busy        = Signal()
+        self.spi_address     = Signal(14)
+        self.spi_write       = Signal()
+        self.spi_write_value = Signal(8)
+
+    def elaborate(self, platform):
+        m = Module()
+
+        interface         = self.interface
+        setup             = self.interface.setup
+
+        m.d.comb += self.spi_write.eq(1) # Only support writes for now
+        m.d.sync += self.spi_start.eq(0)
+
+        with m.If(setup.type == USBRequestType.VENDOR):
+            with m.Switch(setup.request):
+
+                with m.Case(self.REQUEST_WRITE_REG):
+
+                    with m.If(setup.received):
+                        with m.If(self.spi_busy):
+                            # TODO: set flag so we nak later
+                            pass
+                        with m.Else():
+                            m.d.sync += [
+                                self.spi_address.eq(setup.index[:14]),
+                                self.spi_write_value.eq(setup.value[:8]),
+                                self.spi_start.eq(1),
+                            ],
+
+
+                    # Once the receive is complete, respond with an ACK.
+                    with m.If(interface.rx_ready_for_response):
+                        m.d.comb += interface.handshakes_out.ack.eq(1)
+
+                    # If we reach the status stage, send a ZLP.
+                    with m.If(interface.status_requested):
+                        m.d.comb += self.send_zlp()
+
+
+                with m.Case():
+
+                    #
+                    # Stall unhandled requests.
+                    #
+                    with m.If(interface.status_requested | interface.data_requested):
+                        m.d.comb += interface.handshakes_out.stall.eq(1)
+
+                return m
 
 
 class USBInSpeedTestDevice(Elaboratable):
@@ -56,8 +117,8 @@ class USBInSpeedTestDevice(Elaboratable):
             d.idVendor           = VENDOR_ID
             d.idProduct          = PRODUCT_ID
 
-            d.iManufacturer      = "LUNA"
-            d.iProduct           = "IN speed test"
+            d.iManufacturer      = "GSG"
+            d.iProduct           = "Amalthea receiver"
             d.iSerialNumber      = "no serial"
 
             d.bNumConfigurations = 1
@@ -80,16 +141,43 @@ class USBInSpeedTestDevice(Elaboratable):
     def elaborate(self, platform):
         m = Module()
 
+        radio = platform.request("radio")
+        m.d.comb += radio.rst.eq(0)
+
         # Generate our domain clocks/resets.
         m.submodules.car = platform.clock_domain_generator()
 
         # Create our USB device interface...
-        ulpi = platform.request("target_phy")
+        ulpi = platform.request("host_phy")
         m.submodules.usb = usb = USBDevice(bus=ulpi)
 
         # Add our standard control endpoint to the device.
         descriptors = self.create_descriptors()
-        usb.add_standard_control_endpoint(descriptors)
+        control_ep = usb.add_standard_control_endpoint(descriptors)
+
+        # Add our vendor request handler
+        handler = RadioSPIRequestHandler()
+        control_ep.add_request_handler(handler)
+
+        radio_spi = RadioSPI(clk_freq=120e6)
+        m.submodules += radio_spi
+
+        m.d.comb += [
+            # Output pins
+            radio.sel              .eq(radio_spi.sel),
+            radio.sclk             .eq(radio_spi.sclk),
+            radio.mosi             .eq(radio_spi.mosi),
+
+            # Input pins
+            radio_spi.miso         .eq(radio.miso),
+
+            # Vendor request handler connections
+            radio_spi.start        .eq(handler.spi_start),
+            handler.spi_busy       .eq(radio_spi.busy),
+            radio_spi.address      .eq(handler.spi_address),
+            radio_spi.write        .eq(handler.spi_write),
+            radio_spi.write_value  .eq(handler.spi_write_value),
+        ]
 
         # Add a stream endpoint to our device.
         stream_ep = USBStreamInEndpoint(
@@ -165,6 +253,13 @@ def run_speed_test():
         # ... and claim its bulk interface.
         device.claimInterface(0)
 
+        # 24 -> TXPREP
+        device.controlWrite(usb1.REQUEST_TYPE_VENDOR, 0, 0x3, 0x0203, [])
+
+        # 24 -> RX
+        device.controlWrite(usb1.REQUEST_TYPE_VENDOR, 0, 0x5, 0x0203, [])
+
+        return
         # Submit a set of transfers to perform async comms with.
         active_transfers = []
         for _ in range(TRANSFER_QUEUE_DEPTH):
@@ -181,8 +276,8 @@ def run_speed_test():
         start_time = time.time()
 
         # Submit our transfers all at once.
-        for transfer in active_transfers:
-            transfer.submit()
+        #for transfer in active_transfers:
+        #    transfer.submit()
 
         # Run our transfers until we get enough data.
         while not _should_terminate():
@@ -204,17 +299,17 @@ def run_speed_test():
 
 
         bytes_per_second = total_data_exchanged / elapsed
-        logging.info(f"Exchanged {total_data_exchanged / 1000000}MB total at {bytes_per_second / 1000000}MB/s.")
+        logging.warning(f"Exchanged {total_data_exchanged / 1000000}MB total at {bytes_per_second / 1000000}MB/s.")
 
 
 if __name__ == "__main__":
     device = top_level_cli(USBInSpeedTestDevice)
 
-    logging.info("Giving the device time to connect...")
-    time.sleep(5)
+    logging.warning("Giving the device time to connect...")
+    time.sleep(10)
 
     if device is not None:
-        logging.info(f"Starting bulk in speed test.")
+        logging.warning(f"Starting bulk in speed test.")
         run_speed_test()
 
 
