9b31c65af69a629a4019cd92e08a15727f30f270
Mike Walters <mike@flomp.net>
2020-10-22 20:08:09 +0100

    Python packaging

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+
+    # Vitals
+    name='amalthea',
+    license='BSD',
+    url='https://github.com/greatscottgadgets/amalthea',
+    author='Mike Walters',
+    author_email='mike@flomp.net',
+    description='',
+    use_scm_version= {
+        "root": '..',
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": lambda version : version.format_choice("+{node}", "+{node}.dirty"),
+        "fallback_version": "r0.0"
+    },
+
+    # Imports / exports / requirements.
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires="~=3.7",
+    install_requires=[
+        'nmigen @ git+https://github.com/nmigen/nmigen.git',
+        'nmigen_boards @ git+https://github.com/nmigen/nmigen-boards.git',
+        'luna @ git+https://github.com/greatscottgadgets/luna.git',
+    ],
+    setup_requires=['setuptools', 'setuptools_scm'],
+
+    # Metadata
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Security',
+        ],
+)
