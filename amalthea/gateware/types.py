d4649139db83fb1c854d2a930e0d59584f00c302
Mike Walters <mike@flomp.net>
2021-05-21 19:16:24 +0100

    gateware.types: add Complex type

---
+++ amalthea/gateware/types.py
@@ -0,0 +1,30 @@
+from nmigen import *
+
+class Complex(Record):
+    def __init__(self, sample_width):
+        self.sample_width = sample_width
+        super().__init__([
+            ('real', signed(sample_width)),
+            ('imag', signed(sample_width)),
+        ])
+    
+    def __add__(self, other):
+        width = max(self.sample_width, other.sample_width) + 1
+        result = Complex(width)
+        result.real = self.real + other.real
+        result.imag = self.imag + other.imag
+        return result
+
+    def __sub__(self, other):
+        width = max(self.sample_width, other.sample_width) + 1
+        result = Complex(width)
+        result.real = self.real - other.real
+        result.imag = self.imag - other.imag
+        return result
+    
+    def __mul__(self, other):
+        width = self.sample_width + other.sample_width
+        result = Complex(width)
+        result.real = self.real * other.real - self.imag * other.imag
+        result.imag = self.real * other.imag - self.imag * other.real
+        return result
