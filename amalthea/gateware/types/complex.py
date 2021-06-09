5058edb8d85cf8a40a0aa0f3773e31d498b675a2
Mike Walters <mike@flomp.net>
2021-06-09 01:07:16 +0100

    gateware.types.complex: more helpers for making connections & sim testing

--- amalthea/gateware/types/complex.py
+++ amalthea/gateware/types/complex.py
@@ -22,15 +22,39 @@ class Complex:
             self.real = value[0]
             self.imag = value[1]
             assert self.real.shape == self.imag.shape
+            self.shape = self.real.shape
+        elif isinstance(value, Value):
+            assert len(value) == 2*len(shape)
+            l = int(len(value)/2)
+            real = value[0:l]
+            imag = value[l:]
+            if shape.signed:
+                real = real.as_signed()
+                imag = imag.as_signed()
+            self.real = shape.value(real)
+            self.imag = shape.value(imag)
+            self.shape = shape
         else:
             raise TypeError(f"unsupported value {type(value)}")
 
 
     def eq(self, other):
+        assert self.shape == other.shape, f"{self.shape} != {other.shape}"
         return [
             self.real.eq(other.real),
             self.imag.eq(other.imag),
         ]
+
+    def reshape(self, shape):
+        return Complex(value=(self.real.reshape(shape), self.imag.reshape(shape)))
+
+    def to_complex(self):
+        real = (yield from self.real.to_float())
+        imag = (yield from self.imag.to_float())
+        return complex(real, imag)
+
+    def value(self):
+        return Cat(self.real.value, self.imag.value)
 
     def __add__(self, other):
         real = self.real + other.real
